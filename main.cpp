4c1685766aa470be0aade070bc9a8b20a93da051
Madina <42979064+msadirmekova@users.noreply.github.com>
2019-11-27 16:38:16 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -52,35 +52,31 @@ bool check_prefernces(const string city, const DynamicArray<Customer>& searching
 
 int main()
 {
-
+    
     Queue<Customer> waitLine;
-    Queue<Hotel> hotel;
+    Queue<Customer> copy;
+    DynamicArray<Hotel> hotel;
     DynamicArray<Customer> searching;
     DynamicArray<bool> serverStatus;
-    Queue<Hotel> copy_hotel;
-    Queue<Hotel> reserve;
     
-
-
     Customer client;
     Hotel h;
     fstream file;
     fstream user;
     int cust_num = 0;
-
-
+    int index = 0;
+    
     //    string fname, fstar, flocation, fprice, frate;
-
+    
     int xbudget, xstay, xarrival;
-
+    
     char *token;
     char buf[1000];
-    int count;
     const char* const tab = "\t";
-
+    
     char letter = 'Z';
     int classIndex = 0;
-
+    
     user.open("User.txt");
     if(!user.good())
         cout<<"I/O error \n";
@@ -89,116 +85,124 @@ int main()
         string line;
         getline(user, line);
         strcpy(buf, line.c_str());
-
+        
         if(buf[0] == 0)//empty string
             continue;
-
+        
         const string location (token = strtok(buf, tab));
         const string star((token = strtok(0, tab)) ? token : "");
         const string budget((token = strtok(0, tab)) ? token : "");
         const string rate((token = strtok(0, tab)) ? token : "");
         const string arrival((token = strtok(0, tab)) ? token : "");
         const string stayLength((token = strtok(0, tab)) ? token : "");
-
+        
         client.Id = getLetter(letter);
-        cout<<"Customer: "<<client.Id<<endl;
-        cout<<"Enter the city: \n";
-        cout<<location<<endl;
         client.City = location;
-        cout<<"How many stars: \n";
-        cout<<star<<endl;
         client.Star = star;
-        cout<<"Enter the budget: \n";
-        cout<<budget<<endl;
         xbudget = stoi(budget);
         client.Budget = xbudget;
-        //        cout<<"The rate should be above: \n";
-        //        cout<<rate<<endl;
-        cout<<"What is the arrival time: \n";
-        cout<<arrival<<" of November"<<endl;
-
         xarrival = stoi(arrival);
         client.ArrivalTime = xarrival;
-
-        cout<<"Check out date? \n";
-        cout<<stayLength<<" of November"<<endl;
-
         xstay = stoi(stayLength);
         client.endTime = xstay;
         waitLine.push(client);
         cout<<endl;
-//        user.close();
-        //--------------
-        file.open("Hotel.txt");
-        if(!file.good())
-            cout<<"I/O error \n";
+        //        user.close();
+    }//end of the user loop
+    copy = waitLine;
+    //--------------
+    file.open("Hotel.txt");
+    if(!file.good())
+        cout<<"I/O error \n";
+    
+    
+    while(file.good())//hotel
+    {
+        string line;
+        getline(file,line);
+        strcpy(buf, line.c_str());
+        
+        
+        if(buf[0] == 0)//empty string
+            continue;
+        const string fname (token = strtok(buf, tab));
+        const string fstar((token = strtok(0, tab)) ? token : "");
+        const string flocation((token = strtok(0, tab)) ? token : "");
+        const string fprice((token = strtok(0, tab)) ? token : "");
+        const string frate((token = strtok(0, tab)) ? token : "");
+        int con_fprice = stoi(fprice);
+        
+        hotel[index].name = fname;
+        hotel[index].price = con_fprice;
+        hotel[index].rate = frate;
+        hotel[index].star = fstar;
+        hotel[index].location = flocation;
+        index++;
+    }
+    
+    int print = 0;
+    while(!waitLine.empty())
+    {
+        cout<<"Customer: "<<waitLine.front().Id<<endl;
+        cout<<"Enter the city: \n";
+        cout<<waitLine.front().City<<endl;
+        cout<<"How many stars: \n";
+        cout<<waitLine.front().Star<<endl;
+        cout<<"Enter the budget: \n";
+        cout<<waitLine.front().Budget<<endl;
+        //        cout<<"The rate should be above: \n";
+        //        cout<<rate<<endl;
+        
+        cout<<"What is the arrival time: \n";
+        cout<<waitLine.front().ArrivalTime<<" of November"<<endl;
+        cout<<"Check out date? \n";
+        cout<<waitLine.front().endTime<<" of November"<<endl;
 
-        while(file.good())
+cout<<"Available options in "<<waitLine.front().City<<" are: "<<endl;
+        
+        for(int i = 0; i < index; i++)
         {
-            string line;
-            getline(file,line);
-            count++;
-            strcpy(buf, line.c_str());
-
-
-            if(buf[0] == 0)//empty string
-                continue;
-            const string fname (token = strtok(buf, tab));
-            const string fstar((token = strtok(0, tab)) ? token : "");
-            const string flocation((token = strtok(0, tab)) ? token : "");
-            const string fprice((token = strtok(0, tab)) ? token : "");
-            const string frate((token = strtok(0, tab)) ? token : "");
-            int con_fprice = stoi(fprice);
-
-            h.name = fname;
-            h.price = con_fprice;
-            h.rate = frate;
-            h.star = fstar;
-            h.location = flocation;
-            hotel.push(h);
-                        
-            copy_hotel = hotel;
-            reserve = hotel;
-                if(waitLine.front().City == h.location)
+            if(waitLine.front().City == hotel[i].location)
+            {
+                if(waitLine.front().Budget >= hotel[i].price && waitLine.front().Star == hotel[i].star)
                 {
-                    
-                    if(waitLine.front().Budget >= h.price && waitLine.front().Star == h.star)
-                    {
-                        cout<<"Available options in "<<waitLine.front().City<<" are: "<<endl;
-                        cout<<copy_hotel.front().name<<endl<<endl;
-                        
-                    }
+                    cout<<hotel[i].name<<endl<<endl;
                 }
-                else
-                    continue;
-                copy_hotel.pop();
-
-            
-        }//end of the hotel loop
+            }
+            else
+                break;
+        }
         
         cout<<"Enter your choice \n";
         string input;
         cin>>input;
-
-        if(input == reserve.front().name) //reserve
+        for(int i = 0; i < index; i++)
         {
-            for(int i = waitLine.front().ArrivalTime; i != waitLine.front().endTime; i++)
+            if(input == hotel[i].name) //reserve//can I compare two strings like that?
             {
-                serverStatus[i] = true;
+                for(int i = waitLine.front().ArrivalTime; i != waitLine.front().endTime; i++)
+                {
+                    serverStatus[i] = true;
+                }
+                cout << "hello";
+
+                print = 1;
             }
-            cout<<"Succesfully booked \n";
-        }
-        else
-            cout<<"Sorry, your booking is unavailable \n";
+            else
+            {
+                print = 0;
+                continue;
+            }
             
-        reserve.pop();
-
-        file.close();
-
-    }//end of the user loop
-    waitLine.pop();
-
+            if(print == 1)
+            cout<<"Succesfully booked \n";
+            else if (print == 0)
+            cout<<"Sorry, your booking is unavailable \n";
+        }
+        waitLine.pop();
+    }
+    
+    file.close();
     return 0;
 }
-
 
