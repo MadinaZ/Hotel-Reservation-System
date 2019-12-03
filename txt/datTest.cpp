2af2b1ed9c3bb6078db22baf865d4bcfacf51e4c
Hiroaki Takeuchi <Hiroaki@Hiroaki.local>
2019-12-02 23:52:54 -0800

    test complete

--- txt/datTest.cpp
+++ txt/datTest.cpp
@@ -1,3 +1,5 @@
+#include "Hotels.h"
+
 #include <iostream>
 #include <iomanip>
 #include <fstream>
@@ -10,43 +12,26 @@ using namespace std;
 
 
 
-struct Hotels
-{
-	char name[30];
-	char city[20];
-	int star;
-	double rate;
-	char type1[7];
-	char type2[7];
-	char type3[7];
-	char type4[7];
-	char type5[7];
-	char type6[7];
-	char type7[7];
-	double baseprice[7];
-	int cap[7];
-	int num_rooms[7];
-};
-
 
 int main()
 {
 	srand(time(0));
 
 
-	ofstream keepHotel("newHotel.dat", ios::out | ios::binary);
+	//ofstream keepHotel("newHotel.dat", ios::out | ios::binary);
 	ifstream readHotel("newHotel.dat", ios::in | ios::binary);
 	ofstream outHotel("copydatHotel.txt");
 
 
-	if(!keepHotel)
+
+	/*if(!keepHotel)
 	{
 		cerr << "keepHotel error";
 		exit(EXIT_FAILURE);
-	}
+	}*/
 	if(!readHotel)
 	{
-		cerr << "keepHotel error";
+		cerr << "readHotel error";
 		exit(EXIT_FAILURE);
 	}
 	if(!outHotel)
@@ -56,11 +41,22 @@ int main()
 	}
 
 
+	int counter =0;
 	Hotels hotel;
+  readHotel.seekg(0);
 	readHotel.read(reinterpret_cast <char*>(&hotel), sizeof(Hotels));
 
-	while(readHotel && !readHotel.eof())
-		cout << hotel.star << endl;
+	cout << hotel.name;
+
+	while(!readHotel.eof() && readHotel)
+	{
+    cout << hotel.name<< hotel.star << endl;
+
+		readHotel.read(reinterpret_cast<char*>(&hotel), sizeof(Hotels));
+		counter++;
+	}
+
+	cout<< counter;
 
 	return 0;
 }
