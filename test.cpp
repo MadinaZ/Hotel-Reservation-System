dee78b7bfc7f4df7a25e1757cdba73915aef606e
Hiroaki Takeuchi <Hiroaki@Hiroaki.local>
2019-12-02 23:51:56 -0800

    change data structure design

--- test.cpp
+++ test.cpp
@@ -18,16 +18,17 @@ using namespace std;
 
 struct RoomInfo
 {
+  int startDate;
 	int endDate;
 	int customerNum;
 };
 
 struct Rooms
 {
-	bool reserved;
-	int startDate;
-	int userNum;
-	string note;
+  int room_num;
+  string note;
+  PriorityQueue<RoomInfo> eachRoom; // sorted by endDate, pop customers based on endDate
+	bool operator<(const RoomInfo& a, const RoomsInfo b) { return (a.endDate < b.endDate); }
 };
 
 struct RoomTypes
@@ -37,9 +38,8 @@ struct RoomTypes
 	double price() { return baseprice * num_rooms / roomList.size(); } //per night
 	int cap; // max ppl per room
 	int num_rooms;
-	DynamicArray<Rooms> roomList;
-	PriorityQueue<RoomInfo> roomStatus;
-	bool operator<(const RoomInfo& a, const RoomInfo& b) { return (a.endDate < b.endDate); }
+	DynamicArray<Rooms> roomList; // 101,102,103... and so on
+	//PriorityQueue<RoomInfo> roomStatus;
 };
 
 
@@ -51,10 +51,7 @@ struct Hotel
 	double rate;
 	double min_price(int);
 	PriorityQueue<RoomTypes> roomTypes;
-	bool operator<(const RoomTypes& a, const RoomTypes& b)
-	{
-		return (a.cap > b.cap);
-	}
+  bool operator<(const RoomTypes& a, const RoomTypes& b) { return (a.cap > b.cap); }
 };
 
 double Hotel::min_price(int num_ppl)
