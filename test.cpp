7eb7725a96f10e5fe05c1da4f878a89e249fe336
Hiroaki Takeuchi <Hiroaki@Hiroaki.local>
2019-12-03 01:30:50 -0800

    update structure

--- test.cpp
+++ test.cpp
@@ -1,9 +1,9 @@
 #include "AssociativeArray.h"
 #include "DynamicArray.h"
-#include "StaticArray.h"
-#include "Queue.h"
 #include "PriorityQueue.h"
 
+#include "RoomManage.h"
+#include "Hotels.h"
 
 
 #include <iostream>
@@ -16,77 +16,31 @@ using namespace std;
 #include <cmath>
 
 
-struct RoomInfo
-{
-  int startDate;
-	int endDate;
-	int customerNum;
-};
-
-struct Rooms
-{
-  int room_num;
-  string note;
-  PriorityQueue<RoomInfo> eachRoom; // sorted by endDate, pop customers based on endDate
-	bool operator<(const RoomInfo& a, const RoomsInfo b) { return (a.endDate < b.endDate); }
-};
-
-struct RoomTypes
-{
-	string name;
-	double baseprice; // per night
-	double price() { return baseprice * num_rooms / roomList.size(); } //per night
-	int cap; // max ppl per room
-	int num_rooms;
-	DynamicArray<Rooms> roomList; // 101,102,103... and so on
-	//PriorityQueue<RoomInfo> roomStatus;
-};
-
-
-struct Hotel
-{
-	string name;
-	string city;
-	int star;
-	double rate;
-	double min_price(int);
-	PriorityQueue<RoomTypes> roomTypes;
-  bool operator<(const RoomTypes& a, const RoomTypes& b) { return (a.cap > b.cap); }
-};
-
-double Hotel::min_price(int num_ppl)
-{
-	while(roomTypes.cap < num_ppl)
-		roomTypes.pop();
-	return roomTypes.top().price();
-}
-
 
 
 int main()
 {
 
+	AssociativeArray<string, Hotel> hotels;  //key: city
+
+
+	ifstream inHotel("newHotel.dat", ios::in | ios::binary);
 
 
 
-  AssociativeArray<string, Hotel> hotels;
+	Hotels hotel;
+  inHotel.seekg(0);
+  inHotel.read(reinterpret_cast <char*>(&hotel), sizeof(Hotels));
 
-  ifstream fin;
-  fin.open("newHotel.txt");
+  cout << hotel.name;
+  int counter = 0;
+  while(!inHotel.eof() && inHotel)
+  {
+    cout << hotel.name<< hotel.star << endl;
 
-
-	if(!fin)
-	{
-		cerr << "fin error";
-		exit(EXIT_FAILURE);
-	}
-
-	string buf;
-
-	fin >> buf; cout << buf;
-
-
-
+    inHotel.read(reinterpret_cast<char*>(&hotel), sizeof(Hotels));
+    counter++;
+  }
 
 	return 0;
 }
