0e33076af68679dada9a1fefbcb5cda1d424c7b4
Hiroaki Takeuchi <Hiroaki@Hiroaki.local>
2019-12-04 21:59:03 -0800

    cheapeset hotel in the city

--- test.cpp
+++ test.cpp
@@ -2,8 +2,11 @@
 #include "DynamicArray.h"
 #include "PriorityQueue.h"
 #include "BinarySearchTree.h"
+#include "StaticPQ.h"
+#include "StaticArray.h"
 
 #include "Hotels.h"
+#include "ReserveInfo.h"
 //#include "RoomManage.h"
 
 
@@ -12,31 +15,34 @@
 #include <fstream>
 #include <string>
 #include <cstdlib>
+#include <list>
 #include <set>
 using namespace std;
 
 #include <cmath>
 
 
-struct RoomInfo
-{
-  int startDate;
-  int endDate;
-  int customerNum;
 
-  RoomInfo():startDate(),endDate(),customerNum(){};
-  RoomInfo(int a,int b,int c):startDate(a),endDate(b),customerNum(c){};
-};
-bool operator<(const RoomInfo& a, const RoomInfo& b) { return (a.endDate < b.endDate); }
 
 struct Rooms
 {
   int room_num;
   string note;
-  PriorityQueue<RoomInfo> eachRoom; // sorted by endDate, pop customers based on endDate
+  //StaticPQ<RoomInfo,10> eachRoom; // sorted by endDate, pop customers based on endDate / 400 is enough for 365 days?
 
-  Rooms():room_num(),note(),eachRoom(){};
-  Rooms(int a):room_num(a),note(),eachRoom(){};
+  Rooms():room_num(33),note(){};//,eachRoom(){};
+  Rooms(int a):room_num(a),note(){}; //,eachRoom(){};
+
+  Rooms& operator=(const Rooms& ori)
+  {
+  	if(this == &ori) return *this;
+
+  	room_num = ori.room_num;
+  	note = ori.note;
+  	//eachRoom = ori.eachRoom;
+
+  	return *this;
+  }
 };
 bool operator<(const Rooms& a, const Rooms& b) { return (a.room_num < b.room_num); }
 
@@ -50,12 +56,27 @@ struct RoomTypes
   int cap; // max ppl per room
   int num_rooms;
 
-  double price() { return baseprice * num_rooms / roomList.size(); } // need update //per night
-  DynamicArray<Rooms> roomList; // 101,102,103... and so on
+  double price() { return baseprice * num_rooms / roomList.capacity(); } // need update //per night
+  //DynamicArray<Rooms> roomList; // 101,102,103... and so on
+  StaticArray<Rooms,100> roomList;
   //PriorityQueue<RoomInfo> roomStatus;
 
   RoomTypes():name_rt(),baseprice(),cap(),num_rooms(),roomList(){};
   RoomTypes(string a,double b,int c,int d):name_rt(a),baseprice(b),cap(c),num_rooms(d), roomList(){};
+
+  RoomTypes& operator=(const RoomTypes& ori)
+	{
+  	if(this == &ori) return *this;
+
+  	name_rt = ori.name_rt;
+  	baseprice = ori.baseprice;
+  	cap = ori.cap;
+  	num_rooms = ori.num_rooms;
+  	roomList = ori.roomList;
+
+  	return *this;
+	}
+
 };
 bool operator<(const RoomTypes& a, const RoomTypes& b) { return (a.cap > b.cap); }
 
@@ -67,10 +88,23 @@ struct Hotel
   int star;
   double rate;
   double min_price(int);
-  PriorityQueue<RoomTypes> roomTypes;
+  StaticPQ<RoomTypes,10> roomTypes;
 
   Hotel():name_h(),city(),star(),rate(){};
-  Hotel(string a,string b,int c,double d):name_h(a),city(b),star(c),rate(d){};
+  Hotel(string a,string b,int c,double d):name_h(a),city(b),star(c),rate(d), roomTypes(){};
+
+  Hotel& operator=(const Hotel& ori)
+  {
+  	if(this == &ori) return *this;
+
+  	name_h = ori.name_h;
+  	city = ori.city;
+  	star = ori.star;
+  	rate = ori.rate;
+  	roomTypes = ori.roomTypes;
+
+  	return *this;
+  }
 };
 bool operator<(const Hotel& a, const Hotel& b) { return (a.city < b.city); }
 
@@ -90,7 +124,7 @@ int main()
 {
 
 
-	BinarySearchTree<string, PriorityQueue<Hotel> > hotels; // <string, set<Hotel> doesn't work ....
+	BinarySearchTree<string, list<Hotel> > hotels;
 	//AssociativeArray<string, set<Hotel> > hotels2;  //key: city
 
 
@@ -108,9 +142,8 @@ int main()
   {
     //cout << hotel.name<< hotel.star << endl;
 
-
-
     Hotel temp(hotel.name,hotel.city,hotel.star,hotel.rate); // create temp hotel
+
 
     //store 7 types info
     RoomTypes temp1(hotel.type1,hotel.baseprice[0],hotel.cap[0],hotel.num_rooms[0]);
@@ -122,60 +155,95 @@ int main()
     RoomTypes temp7(hotel.type7,hotel.baseprice[6],hotel.cap[6],hotel.num_rooms[6]);
 
 
-    //store each room type
     for(int i = 0 ; i < temp1.num_rooms; i++)
-    {
-    	Rooms temp_rm(i);
-    	temp1.roomList[i] = temp_rm;
-    }
+    	temp1.roomList[i].room_num = i;
     for(int i = 0 ; i < temp2.num_rooms; i++)
-    {
-    	Rooms temp_rm(i);
-    	temp2.roomList[i] = temp_rm;
-    }
+    	temp2.roomList[i].room_num = i;
     for(int i = 0 ; i < temp3.num_rooms; i++)
-    {
-    	Rooms temp_rm(i);
-    	temp3.roomList[i] = temp_rm;
-    }
+    	temp3.roomList[i].room_num = i;
     for(int i = 0 ; i < temp4.num_rooms; i++)
-    {
-    	Rooms temp_rm(i);
-    	temp4.roomList[i] = temp_rm;
-    }
+    	temp4.roomList[i].room_num = i;
     for(int i = 0 ; i < temp5.num_rooms; i++)
-    {
-    	Rooms temp_rm(i);
-    	temp5.roomList[i] = temp_rm;
-    }
+    	temp5.roomList[i].room_num = i;
     for(int i = 0 ; i < temp6.num_rooms; i++)
-    {
-    	Rooms temp_rm(i);
-    	temp6.roomList[i] = temp_rm;
-    }
+    	temp6.roomList[i].room_num = i;
     for(int i = 0 ; i < temp7.num_rooms; i++)
-    {
-    	Rooms temp_rm(i);
-    	temp7.roomList[i] = temp_rm;
-    }
+    	temp1.roomList[i].room_num = i;
+
+    //store roomtype
+    temp.roomTypes.push(temp1);
+    temp.roomTypes.push(temp2);
+    temp.roomTypes.push(temp3);
+    temp.roomTypes.push(temp4);
+    temp.roomTypes.push(temp5);
+    temp.roomTypes.push(temp6);
+    temp.roomTypes.push(temp7);
 
 
-
-    cout << temp.city << temp.name_h << endl;
-    hotels[temp.city].push(temp);
+    //hotels[temp.city].push(temp);
+    hotels[temp.city].push_back(temp);
 
     inHotel.read(reinterpret_cast<char*>(&hotel), sizeof(Hotels));
    // hotels2[temp.city].insert(temp);
   } // finish storing hotel info
 
 //  cout << hotels.size();
-  cout << hotels["San Francisco"].top().name_h;
-  hotels["San Francisco"].pop();
-  cout << hotels["San Francisco"].top().name_h;
-/*
-  set<Hotel>::iterator it;
-  for(it = hotels["San Francisco"].begin(); it != hotels["San Francisco"].end(); it++)
-  	cout << it->name_h << endl;*/
+//  cout << hotels["San Francisco"].top();
+ // hotels["San Francisco"].pop();
+ // cout << hotels["San Francisco"].top().name_h;
+  cout<<hotels["San Francisco"].size();
+  list<Hotel>::iterator it;
+  for(it = hotels["New York"].begin(); it != hotels["New York"].end(); it++)
+  	cout << it->name_h << endl;
+
+
+  string city, buf;
+  int num_p;
+
+  cout << "City? ";
+  getline(cin, city);
+  while(hotels[city].size() == 0)
+  {
+  	cout << "No hotel found in the city" << endl;
+    cout << "City? ";
+    getline(cin, city);
+  }
+
+  cout << "How many people? " << endl;
+  getline(cin, buf);
+  num_p = atoi(buf.c_str());
+  while(num_p == 0)
+  {
+  	cout << "need to be more than 0" << endl;
+  	cout << "How many people? " << endl;
+    getline(cin, buf);
+    num_p = atoi(buf.c_str());
+  }
+ 	set<pair<double,string> > sortprice;
+  //look for the cheapest price in the user condition
+  for(it = hotels[city].begin(); it != hotels[city].end(); it++)
+  {
+  	while(it->roomTypes.top().baseprice == 0) // remove 0 dollar hotel roomtypes
+    		it->roomTypes.pop();
+  	while(it->roomTypes.top().cap >= num_p) // pop roomytypes with a cap less than user input
+  		it->roomTypes.pop();
+
+
+
+  	pair<double,string> temp;
+  	temp.first = it->roomTypes.top().baseprice;
+  	temp.second = it->name_h;
+  	sortprice.insert(temp);
+  }
+  cout << "The cheapest hotel in " << city << "\n\t" << sortprice.begin()->second << " from $"
+  		 << fixed << setprecision(2) << sortprice.begin()->first << endl;
+
+
+
+
+
+
+
 
 
   cout << "finish" << endl;
